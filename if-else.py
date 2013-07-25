d76cc3f0499961425fce228fe7d342d11892e862
Oz Akan <ozgurakan@gmail.com>
2013-07-25 14:37:55 -0500

    if else example

---
+++ if-else.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+# if / else example
+
+number = 10
+
+if number > 0:
+    print "number is positive"
+
+if number > 0:
+    print "number is positive"
+else:
+    print "number is negatife"
+
+if number > 5:
+    print "number is bigger than 5"
+elif number == 5:
+    print "number is equal to 5"
+else:
+    print "number is smaller than 5"
